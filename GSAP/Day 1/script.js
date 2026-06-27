a1d4916aaa3c30f9f2d4fa0d6e6a6b804ce5416e
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-06-27 07:50:01 +0530

    DAY 1 of  HTML, CSS, and JavaScript files for GSAP animations

---
+++ GSAP/Day 1/script.js
@@ -0,0 +1,73 @@
+// let box = document.querySelectorAll(".box")
+// let circle = document.querySelector(".circle")
+
+// gsap.to([box, circle], {
+//     x:500,
+//     duration:1,
+//     delay:1
+// })
+// let box = document.querySelector(".box")
+// let circle = document.querySelector(".circle")
+
+// gsap.to([box, circle], {
+//     x:500,
+//     duration:1,
+//     delay:1
+// })
+// let box = document.querySelectorAll(".box")
+
+// gsap.to(box, {
+//     x:500,
+//     duration:1,
+//     delay:1
+// })
+
+// gsap.to(".box", {
+//   x: 500,
+//   duration: 1,
+//   delay: 1,
+// });
+
+// gsap.to([".box" , ".circle"], {
+//   x: 500,
+//   duration: 1,
+//   delay: 1,
+// });
+// gsap.from([".box" , ".circle"], {
+//   x: 500,
+//   duration: 1,
+//   delay: 1,
+// })
+// gsap.fromTo([".box", ".circle"], {
+//     x:100,
+//     y:50,
+// },{
+//     x:400,
+//     y:200,
+//     duration:3,
+//     delay:1
+// })
+
+// gsap.fromTo([".box", ".circle"], {
+//     x:400,
+//     y:200,
+// },{
+//     x:100,
+//     y:50,
+//     duration:3,
+//     delay:4
+// })
+
+// gsap.set(".circle", {
+//   x: 100,
+//   y: 50,
+//   opacity: 0.2,
+// });
+
+// gsap.to(".circle", {
+//   x: 300,
+//   y: 200,
+//   opacity:1,
+//   duration: 3,
+//   delay: 2,
+// });
