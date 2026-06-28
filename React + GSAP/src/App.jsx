db24d933c0f568b31bebd9daa0484db367aae23c
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-06-28 09:06:13 +0530

    Initialize Day 6 --> React project with GSAP integration, add essential files and configurations

---
+++ React + GSAP/src/App.jsx
@@ -0,0 +1,109 @@
+// import React from 'react'
+// import { useRef } from 'react'
+// import {useGSAP} from "@gsap/react"
+// import gsap from 'gsap'
+
+// const App = () => {
+
+//   const boxRef = useRef(null)
+//   useGSAP(() => {
+//     gsap.to(boxRef.current, {
+//       x:500,
+//       ease:"power2.inOut",
+//       delay:1,
+//       duration:0.4
+//     })
+//   }
+//   )
+//   return (
+//     <div>
+//       <div ref={boxRef} className="box"></div>
+//     </div>
+//   )
+// }
+
+// export default App
+// import React from "react";
+// import { useRef } from "react";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+
+// const App = () => {
+//   const boxRef = useRef(null);
+//   const containerRef = useRef(null)
+//   useGSAP(() => {
+//     gsap.to(boxRef.current, {
+//       x: 500,
+//       ease: "power2.inOut",
+//       delay: 1,
+//       duration: 0.4,
+//     });
+//   },{scope: containerRef.current, dependencies:[], revertOnUpdate:true});
+//   return (
+//     <div ref={containerRef}>
+//       <div ref={boxRef} className="box"></div>
+//     </div>
+//   );
+// };
+
+// export default App;
+// import React from "react";
+// import { useRef } from "react";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+
+// const App = () => {
+//   const boxRef = useRef(null);
+//   const containerRef = useRef(null)
+//   const {contextSafe} = useGSAP(() => {
+//     gsap.to(boxRef.current, {
+//       x: 500,
+//       ease: "power2.inOut",
+//       delay: 1,
+//       duration: 0.4,
+//     });
+//   },{scope: containerRef.current, dependencies:[], revertOnUpdate:true});
+//   return (
+//     <div ref={containerRef}>
+//       <div ref={boxRef} className="box"></div>
+//       <button onClick={contextSafe(() => {
+//         gsap.to(boxRef.current,{
+//           x:0,
+//         } )
+//       }
+//       )}>Click ME</button>
+//     </div>
+//   );
+// };
+
+// export default App;
+import React from "react";
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const App = () => {
+  const boxRef = useRef(null);
+  const containerRef = useRef(null)
+  const {contextSafe} = useGSAP(() => {
+    gsap.to(boxRef.current, {
+      x: 500,
+      ease: "power2.inOut",
+      delay: 1,
+      duration: 0.4,
+    });
+  },{scope: containerRef.current, dependencies:[], revertOnUpdate:true});
+  return (
+    <div ref={containerRef}>
+      <div ref={boxRef} className="box"></div>
+      <button onClick={contextSafe(() => {
+        gsap.to(boxRef.current,{
+          x:0,
+        } )
+      }
+      )}>Click ME</button>
+    </div>
+  );
+};
+
+export default App;
