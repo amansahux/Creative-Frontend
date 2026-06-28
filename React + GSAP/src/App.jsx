3cafa37f7426d760c929e7b7691db46cce788d33
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-06-28 16:11:19 +0530

    Day 6  Learn Multi Element Selection on React using GSAP

--- React + GSAP/src/App.jsx
+++ React + GSAP/src/App.jsx
@@ -84,23 +84,58 @@ import gsap from "gsap";
 import Animate from "./components/Animate";
 
 const App = () => {
-  const boxRef = useRef(null);
-  const containerRef = useRef(null)
-   useGSAP(() => {
-    gsap.to(boxRef.current, {
-      x: 500,
-      ease: "power2.inOut",
-      delay: 1,
-      duration: 0.4,
-    });
-  },{scope: containerRef.current, dependencies:[], revertOnUpdate:true});
+  const boxRef = useRef([]);
+  const containerRef = useRef(null);
+  useGSAP(
+    () => {
+      gsap.to([boxRef.current], {
+        x: 500,
+        ease: "power2.inOut",
+        delay: 1,
+        duration: 0.4,
+      });
+    },
+    { scope: containerRef.current, dependencies: [], revertOnUpdate: true },
+  );
   return (
-   <div>
-    <Animate>
-   <div className="box"></div>
-    </Animate>
-   </div>
+    <div>
+      <div ref={(el) => boxRef.current.push(el)} className="box"></div>
+      <div ref={(el) => boxRef.current.push(el)} className="box"></div>
+      <div
+        ref={(el) => {
+          return boxRef.current.push(el);
+        }}
+        className="box"
+      ></div>
+    </div>
   );
 };
 
 export default App;
+// import React from "react";
+// import { useRef } from "react";
+// import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+// import Animate from "./components/Animate";
+
+// const App = () => {
+//   const boxRef = useRef(null);
+//   const containerRef = useRef(null)
+//    useGSAP(() => {
+//     gsap.to(boxRef.current, {
+//       x: 500,
+//       ease: "power2.inOut",
+//       delay: 1,
+//       duration: 0.4,
+//     });
+//   },{scope: containerRef.current, dependencies:[], revertOnUpdate:true});
+//   return (
+//    <div>
+//     <Animate>
+//    <div className="box"></div>
+//     </Animate>
+//    </div>
+//   );
+// };
+
+// export default App;
